fc8180fd44368a3aca44b8a5cc303008597bb50b
Bhavith Manapoty <catchbhav2002@gmail.com>
2022-12-04 23:22:10 -0500

    added basic db and new account

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 import express from "express";
 import minimist from "minimist";
+import Database from "better-sqlite3";
+
+const bcrypt = require("bcrypt");
+const db = new Database("database.db");
+db.pragma("journal_mode = WAL");
+
+try {
+  db.exec(
+    "CREATE TABLE Users (ID INTEGER PRIMARY KEY AUTOINCREMENT, Name VARCHAR(100), E-Mail VARCHAR(255), UserName VARCHAR(64), Password VARCHAR(64));"
+  );
+} catch (error) {}
+
+try {
+  db.exec(
+    "CREATE TABLE Leaderboard (ID INTEGER PRIMARY KEY AUTOINCREMENT, UserName VARCHAR(64), Highest_Score INTEGER);"
+  );
+} catch (error) {}
+
+try {
+  db.exec(
+    "CREATE TABLE Logs (ID INTEGER PRIMARY KEY AUTOINCREMENT, UserName VARCHAR(64), Message VARCHAR, Time VARCHAR);"
+  );
+} catch (error) {}
 
 const app = express();
 const args = minimist(process.argv.slice(2));
@@ -11,3 +34,34 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
   res.redirect("/login");
 });
+
+app.post("/new-account", function (req, res) {
+  //Variables for the input parameters
+  const name = req.body.name;
+  const email = req.body.email;
+  const username = req.body.username;
+  const password = req.body.password;
+
+  //Check if username exists in database
+  const checkUsername = db.prepare(
+    `SELECT * FROM Users WHERE UserName='${username}'`
+  );
+  let result = checkUsername.get();
+
+  if (result === undefined) {
+    bcrypt.hash(password, saltRounds, function (err, hash) {
+      const newAcc = `INSERT INTO Users (Name, E-Mail, UserName, Password) VALUES ('${name}', '${email}', '${username}', '${hash}');`;
+      db.exec(newAcc);
+    });
+    const time = Date.now();
+    const now = new Date(time);
+    const logAccCreate = `INSERT INTO Logs (UserName, Message, Time) VALUES ('${username}', 'created a new account', '${now.toISOString()}');`;
+    db.exec(logAccCreate);
+    //CREATE FILES TO RENDER AT THIS STAGE
+    // res.render("new_acc_made");
+  } else {
+    // res.render("username_exists");
+  }
+});
+
+app.listen(port);
